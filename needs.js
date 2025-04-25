2f09eb4d90c3ac87001a0483d62f3fef3af7b964
iwant22 <robertomanra22@gmail.com>
2025-04-25 11:32:38 -0700

    Mobile test

--- needs.js
+++ needs.js
@@ -24,3 +24,9 @@ window.onload = function () {
         if (icon) icon.textContent = "🌞";
     }
 };
+
+
+function toggleMenu() {
+    const links = document.getElementById("nav-links");
+    links.classList.toggle("open");
+}
